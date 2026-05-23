8adbb95b9d9a2fd80efc92cb816e31f51ba9aab0
Aykhan Ahmadzada <ayxanx17@gmail.com>
2026-05-23 11:32:04 +0300

    feat(shared): add analytics DTOs

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,4 +3,5 @@ export * from './dto/pagination';
 export * from './dto/auth';
 export * from './dto/catalog';
 export * from './dto/history';
+export * from './dto/analytics';
 export * from './constants';
